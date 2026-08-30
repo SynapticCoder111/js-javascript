533a512355bb3363ccce96e792147f06be25ea49
Prince Kumar <prinsep97@gmail.com>
2026-08-30 18:14:46 +0000

    Conversion

--- 01_basics/03_datatypeConversion.js
+++ 01_basics/03_datatypeConversion.js
@@ -7,11 +7,26 @@ let score = "33abc"
 //console.log(valueInNumber)
 
 
-let isLoggedIn = "Prince"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn)
-let Number = 4567
-let changeNumber = String(Number)
-console.log(changeNumber)
-console.log(typeof changeNumber)
+// let isLoggedIn = "Prince"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// // console.log(booleanIsLoggedIn)
+// let Number = 4567
+// let changeNumber = String(Number)
+// console.log(changeNumber)
+// console.log(typeof changeNumber)
+
+
+//     Operations    
+
+// console.log(1+2)
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 +"2")
+
+let gamecounter = 100
+gamecounter++;
+console.log(gamecounter)
+
+
+
 
