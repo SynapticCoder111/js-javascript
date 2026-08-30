9b8e249e94116d3cf3ca378ab930ffd0b576ac83
Prince Kumar <prinsep97@gmail.com>
2026-08-30 13:38:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 123456
+let accountEmail = "prince@gmail.com"
+var accountPassword = "5590"
+accountCity = "Prayagraj"
+let accountState;
+
+
+
+// accountId = 3   not allowed
+
+accountEmail = "shlok@gmail.com"
+accountPassword = "6392"
+accountCity = "Haridwar"
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all JS code as new version
+console.log("Prince")
+
+let age = 18
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// boolean => true/false
+// bigint => for very large number
+// string => ""
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object 
+console.log(typeof age)
+console.log(typeof null)
+
